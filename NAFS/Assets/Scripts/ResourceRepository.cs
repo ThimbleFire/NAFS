db8ecf19070d11601007ba34dc109c782b8a0f95
Thimble <anthonyeboothroyd@gmail.com>
2023-05-04 02:28:20 +0100

    Update ResourceRepository.cs

--- NAFS/Assets/Scripts/ResourceRepository.cs
+++ NAFS/Assets/Scripts/ResourceRepository.cs
@@ -1,3 +1,18 @@
-public static class ResourceRepository{
+using System;
+using UnityEngine;
 
+public class ResourceRepository<GameObject> : MonoBehaviour
+
+{
+    public GameObject[] resource;
+    
+    public GameObject this[int i]
+
+   {
+
+      get { return resource[i]; }
+
+      //set { resource[i] = value; }
+
+   }
 }
