31bd6abfdc3a4c3a1aea5e47879b23bad46973ab
Thimble <anthonyeboothroyd@gmail.com>
2023-05-05 23:36:00 +0100

    Update pickup.cs

--- NAFS/Assets/Scripts/pickup.cs
+++ NAFS/Assets/Scripts/pickup.cs
@@ -17,7 +17,7 @@ public class pickup : MonoBehaviour
     {
         float distance = Vector3.Distance(position, transform.position);
 
-        if( distance <= 0.32f && Inventory.CountEmptySlots > 0)
+        if( distance <= 0.16f && Inventory.CountEmptySlots > 0)
         {
             pickupable = false;
             PlayerMove.OnMove -= PlayerMove_OnMove;
@@ -29,10 +29,17 @@ public class pickup : MonoBehaviour
         if (pickupable == false)
             transform.position = Vector3.MoveTowards(transform.position, PlayerCharacter.WorldPosition, 0.001f);
 
-        if (Vector3.Distance(PlayerCharacter.WorldPosition, transform.position) <= 0.008f)
+        if (Vector3.Distance(PlayerCharacter.WorldPosition, transform.position) <= 0.01f)
         {
             Inventory.AddItem(itemMono);
             Destroy(gameObject);
         }
+
+if (Vector3.Distance(PlayerCharacter.WorldPosition, transform.position) >= 0.64f)
+        {
+            pickupable = true;
+            PlayerMove.OnMove += PlayerMove_OnMove;
+        }
+
     }
 }
