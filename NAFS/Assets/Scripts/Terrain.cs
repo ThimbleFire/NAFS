1352805c6b391480a37dcc2f789edd2b0b1ee4ce
Thimble <anthonyeboothroyd@gmail.com>
2023-05-05 04:52:50 +0100

    Update Terrain.cs
    Added a note to self

--- NAFS/Assets/Scripts/Terrain.cs
+++ NAFS/Assets/Scripts/Terrain.cs
@@ -27,7 +27,7 @@ public class Terrain : MonoBehaviour
         //You need to make it so multiple game objects can't be placed on the same tile
 
         if (grassLayer.GetTile(Cursor.Position).Equals(tileBaseTilledEarth)) {
-
+        // note: the reason why game object is out of position is due to the sprite origin being centered.
             CropMono seed = Instantiate(ResourceRepository.prefab["Seed"], grassLayer.CellToWorld(Cursor.Position), Quaternion.identity).GetComponent<CropMono>();
             seed.Setup(playerCharactersActiveTool.name);
         }
