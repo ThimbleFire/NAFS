d75d6e077b2e0dee919ce6facec096e8608ee798
Thimble <anthonyeboothroyd@gmail.com>
2023-05-06 05:51:32 +0100

    Update PlayerCharacter.cs

--- NAFS/Assets/Scripts/PlayerCharacter.cs
+++ NAFS/Assets/Scripts/PlayerCharacter.cs
@@ -5,5 +5,5 @@ using UnityEngine;
 public class PlayerCharacter : MonoBehaviour
 {
     public static Vector3 WorldPosition = Vector3.zero;
-    public static Vector3Int FacingPosition = Vector3Int.down;
+    public static Vector3 FacingPosition = Vector3Int.down;
 }
